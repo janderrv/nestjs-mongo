20edb8fd809ecbb5e9ce8a3569ea211a1eca3ce9
janderrv26@gmail.com <janderrv26@gmail.com>
2022-11-05 18:12:39 -0400

    fix: fix import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest')],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest'), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
